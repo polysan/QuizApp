1b22ba1523a4ca87ce4e63859a6c0eae04005bfa
岡本恵太 <okamotokeita@okamotokeitanoMacBook-puro.local>
2019-10-12 11:39:43 +0900

    RegistQuizResultDao.javaをデータソースを使ったDB接続設定に変更。

--- src/DAO/registQuizResultdao.java
+++ src/DAO/registQuizResultdao.java
@@ -1,57 +1,60 @@
 package dao;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
 
 import model.QuizCount;
 import model.User;
 
 public class RegistQuizResultDao {
-	private final String DRIVER_NAME = "org.h2.Driver";
-	private final String JDBC_URL = "jdbc:h2:~/quiz";
-	private final String DB_USER = "sa";
-	private final String DB_PASS = "";
+//	private final String DRIVER_NAME = "org.h2.Driver";
+//	private final String JDBC_URL = "jdbc:h2:~/quiz";
+//	private final String DB_USER = "sa";
+//	private final String DB_PASS = "";
 
 	public boolean resistQuizResult(User user,QuizCount quiz) {
 
-		Connection  conn = null;
-
+		Connection  connection = null;
+		PreparedStatement pStmt = null;
 		try {
-			Class.forName(DRIVER_NAME);
-			conn = DriverManager.getConnection(JDBC_URL, DB_USER, DB_PASS);
+			InitialContext ic = new InitialContext();
+			DataSource ds = (DataSource)ic.lookup("java:/comp/env/jdbc/Quiz");
+			connection = ds.getConnection();
 
-			conn.setAutoCommit(false);
+//			Class.forName(DRIVER_NAME);
+//			conn = DriverManager.getConnection(JDBC_URL, DB_USER, DB_PASS);
+
+			connection.setAutoCommit(false);
 
 			String sql =
 			"INSERT INTO QUIZ_RESULT(CORRECTCOUNT,QUESTIONCOUNT,USERID) VALUES (?,?,?)";
-			PreparedStatement pStmt = conn.prepareStatement(sql);
+			pStmt = connection.prepareStatement(sql);
 
 			pStmt.setInt(1, quiz.getKaitoCount());
 			pStmt.setInt(2, quiz.getQuesCount()-1);
 			pStmt.setInt(3, user.getId());
 			pStmt.executeUpdate();
 
-			conn.commit();
-		}catch(SQLException e){
+			connection.commit();
+		}catch(Exception e){
 			try {
-				conn.rollback();
+				connection.rollback();
 			} catch (SQLException e1) {
-				// TODO 自動生成された catch ブロック
 				e1.printStackTrace();
 			}
 			e.printStackTrace();
-		}catch (ClassNotFoundException e) {
-			e.printStackTrace();
-			return false;
 		}finally {
-			if(conn != null) {
+			if(connection != null) {
 				try {
-					conn.close();
+					pStmt.close();
+					connection.close();
 				}catch(SQLException e) {
 					e.printStackTrace();
-						return false;
+					return false;
 				}
 			}
 		}
