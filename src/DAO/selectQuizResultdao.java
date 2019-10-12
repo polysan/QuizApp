7f83019dac796d64f06d470748f07dfa8a4d1a9d
岡本恵太 <okamotokeita@okamotokeitanoMacBook-puro.local>
2019-10-12 11:57:17 +0900

    SelectQuizResultDao.javaをデータソースを使ったDB接続設定に変更。

--- src/DAO/selectQuizResultdao.java
+++ src/DAO/selectQuizResultdao.java
@@ -1,7 +1,6 @@
 package dao;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -10,28 +9,36 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
 import model.User;
 
 public class SelectQuizResultDao {
-	private final String DRIVER_NAME = "org.h2.Driver";
-	private final String JDBC_URL = "jdbc:h2:~/quiz";
-	private final String DB_USER = "sa";
-	private final String DB_PASS = "";
+//	private final String DRIVER_NAME = "org.h2.Driver";
+//	private final String JDBC_URL = "jdbc:h2:~/quiz";
+//	private final String DB_USER = "sa";
+//	private final String DB_PASS = "";
 
 	public Map<List <Integer>,List <Integer>> selectQuizResult(User user) {
 
-		Connection  conn = null;
+		Connection  connection = null;
+		PreparedStatement pStmt = null;
 
 		List <Integer> ansCount = new ArrayList <Integer>();
 		List <Integer> quesCount = new ArrayList <Integer>();
 		Map<List <Integer>,List <Integer>> qizResultMap = new HashMap<List <Integer>,List <Integer>>();
 		try {
-			Class.forName(DRIVER_NAME);
-			conn = DriverManager.getConnection(JDBC_URL, DB_USER, DB_PASS);
+			InitialContext ic = new InitialContext();
+			DataSource ds = (DataSource)ic.lookup("java:/comp/env/jdbc/Quiz");
+			connection = ds.getConnection();
+
+//			Class.forName(DRIVER_NAME);
+//			conn = DriverManager.getConnection(JDBC_URL, DB_USER, DB_PASS);
 
 			String sql =
 			"SELECT CORRECTCOUNT,QUESTIONCOUNT FROM QUIZ_RESULT WHERE USERID =?";
-			PreparedStatement pStmt = conn.prepareStatement(sql);
+			pStmt = connection.prepareStatement(sql);
 
 			pStmt.setInt(1, user.getId());
 			ResultSet rs = pStmt.executeQuery();
@@ -43,16 +50,14 @@ public class SelectQuizResultDao {
 				}
 			qizResultMap.put(ansCount, quesCount);
 
-		}catch(SQLException e){
-				e.printStackTrace();
-				return null;
-		}catch (ClassNotFoundException e) {
+		}catch(Exception e){
 				e.printStackTrace();
 				return null;
 		}finally {
-			if(conn != null) {
+			if(connection != null) {
 				try {
-					conn.close();
+					pStmt.close();
+					connection.close();
 				}catch(SQLException e) {
 					e.printStackTrace();
 					return null;
