1ac692249357ee0d05063e71ecadd4ae8a5785ad
岡本恵太 <okamotokeita@okamotokeitanoMacBook-puro.local>
2019-10-12 11:46:39 +0900

    RegistUserDao.javaをデータソースを使ったDB接続設定に変更。

--- src/DAO/registuserdao.java
+++ src/DAO/registuserdao.java
@@ -1,55 +1,59 @@
 package dao;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
 
 import model.User;
 
 public class RegistUserDao {
-	private final String DRIVER_NAME = "org.h2.Driver";
-	private final String JDBC_URL = "jdbc:h2:~/quiz";
-	private final String DB_USER = "sa";
-	private final String DB_PASS = "";
+//	private final String DRIVER_NAME = "org.h2.Driver";
+//	private final String JDBC_URL = "jdbc:h2:~/quiz";
+//	private final String DB_USER = "sa";
+//	private final String DB_PASS = "";
 
 	public boolean newRegist(User user) {
-		Connection  conn = null;
+		Connection  connection = null;
+		PreparedStatement pStmt = null;
 
 		try {
-			Class.forName(DRIVER_NAME);
-			conn = DriverManager.getConnection(JDBC_URL, DB_USER, DB_PASS);
+			InitialContext ic = new InitialContext();
+			DataSource ds = (DataSource)ic.lookup("java:/comp/env/jdbc/Quiz");
+			connection = ds.getConnection();
 
-			conn.setAutoCommit(false);
+//			Class.forName(DRIVER_NAME);
+//			conn = DriverManager.getConnection(JDBC_URL, DB_USER, DB_PASS);
+
+			connection.setAutoCommit(false);
 
 			String sql =
 			"INSERT INTO USER (NAME,PASSWORD) VALUES (?,?)";
-			PreparedStatement pStmt = conn.prepareStatement(sql);
+			pStmt = connection.prepareStatement(sql);
 
 			pStmt.setString(1, user.getName());
 			pStmt.setString(2, user.getPass());
 			pStmt.executeUpdate();
 
-			conn.commit();
-		}catch(SQLException e){
+			connection.commit();
+		}catch(Exception e){
 			try {
-				conn.rollback();
+				connection.rollback();
 			} catch (SQLException e1) {
-				// TODO 自動生成された catch ブロック
 				e1.printStackTrace();
 			}
 				e.printStackTrace();
 				return false;
-		}catch (ClassNotFoundException e) {
-				e.printStackTrace();
-				return false;
 		}finally {
-			if(conn != null) {
+			if(connection != null) {
 				try {
-					conn.close();
+					pStmt.close();
+					connection.close();
 				}catch(SQLException e) {
 					e.printStackTrace();
-						return false;
+					return false;
 				}
 			}
 		}
